e7acad865936ba0e6a837486fbae8429c4983db5
Prince Canuma <prince.gdt@gmail.com>
2024-07-09 14:26:31 +0200

    remove unused files

--- fastmlx/common.py
+++
@@ -1,7 +0,0 @@
-"""The common module contains common functions and classes used by the other modules.
-"""
-
-def hello_world():
-    """Prints "Hello World!" to the console.
-    """
-    print("Hello World!")
